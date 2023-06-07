9407950f6073cffc44ae2d80ac88c19216ebfb74
Christopher Caldwell <62559469+christopher-caldwell@users.noreply.github.com>
2023-06-07 12:08:50 -0500

    Cleaning up sloppiness (#7)
    * feat: Adding Vite example
    
    * fix: Removing Vite example
    
    * fix: Cleaning up configs

--- src/components/inline-suggest/index.tsx
+++ src/components/inline-suggest/index.tsx
@@ -3,7 +3,7 @@ import { styled, TextField } from '@mui/material'
 import { Suggestion } from '../suggestion'
 import { useInlineSuggest, Props } from './useInlineSuggest'
 
-export const InlineSuggest = function <T>(props: Props<T>) {
+export const InlineSuggest = function <TSuggestion>(props: Props<TSuggestion>) {
   const { shouldRenderSuggestion, textFieldProps } = props
   const { value, isFocused, getNeedle, ...restHandlers } = useInlineSuggest(props)
   return (
